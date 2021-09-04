e039f964768ebb5da96190233c8bc7af7c6f4889
rmsharp <rmsharp@me.com>
2021-09-04 18:36:29 -0500

    added DESCRIPION info, LICENSE, LICENSE.md, docs, and edited narrative

---
+++ R/make_complete_sample_durations.R
@@ -0,0 +1,17 @@
+#' Make a complete sample duration data.frame object
+#'
+#' @returns an sample duration data.frame object
+#' @param sample_duration named numeric vector that may be missing actions if
+#' those actions did not occur during the sampling phase.
+#' @param actor_activities data.frame of all possible actor actions.
+#' @export
+make_complete_sample_durations <- function(sample_duration, actor_activities) {
+  description <- names(sample_duration)
+  missing_names <- actor_activities$action[
+    !is.element(actor_activities$action, description)]
+  missing_names_len <- length(missing_names)
+  description <- c(description, missing_names)
+  duration <- as.numeric(sample_duration)
+  duration <- c(duration, rep(0, missing_names_len))
+  data.frame(description = description, duration = duration)
+}
